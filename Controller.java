3bafbe4fce8a5fc592fa5c357b5a9f3d49347d63
Pozdnyakov Anton <annihilator61@mail.ru>
2021-05-31 21:51:51 +0300

    new voids setPlaintext() & getPlainText()

--- Controller.java
+++ Controller.java
@@ -2,11 +2,15 @@ package com.javarush.task.task32.task3209;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultStyledDocument;
 import javax.swing.text.Document;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 public class Controller {
     private View view;
@@ -23,6 +27,26 @@ public class Controller {
 
     public void init() {
 
+    }
+
+    public String getPlainText() {
+        StringWriter writer = new StringWriter();
+        try {
+            new HTMLEditorKit().write(writer, document, 0,document.getLength());
+        } catch (IOException | BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
+            return writer.toString();
+    }
+
+    public void setPlainText(String text) {
+        resetDocument();
+        ;
+        try (StringReader reader = new StringReader(text)){
+            new HTMLEditorKit().read(reader, document, 0);
+        } catch (IOException | BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
     }
 
     public void resetDocument() {
